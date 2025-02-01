3643796e3799d8f2c9e8edf68f82d3923e383612
Pedro Henrique <pedrohr0715@gmail.com>
2025-02-01 12:27:10 -0300

    Começando a página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
-import imagem from './assets/images/logo-conexao-vip.webp';
 import Header from './components/Header/Header';
+import PageInity from './components/Pages/PageInity/Index';
 
 function App() {
 
   return (
     <>
       <Header />
+      <PageInity />
     </>
   )
 }
