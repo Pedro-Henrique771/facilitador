ab36887095117332168d984cebd8228b0dcdc7d7
Pedro Henrique <pedrohr0715@gmail.com>
2025-02-04 11:16:21 -0300

    Começando adicionar funcionalidade

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>Criado por <a href="https://github.com/Pedro-Henrique771">Pedro de Jesus</a></span>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Pages/PageInity/Index.tsx
+++ src/components/Pages/PageInity/Index.tsx
@@ -1,27 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './PageInity.css'
-const PageInity = () => {
-  return (
-    <form action="" className='formulario'>
-        <h1>Selecione o tipo de atendimento que está se iniciando...</h1>
-        <div className='tipoatendimento'>
-            <input type="checkbox" name="sem-acesso" id="sem-acesso" />
-            <label htmlFor="sem-acesso">Cliente alega sem acesso</label>
-        </div>
-        <div className='tipoatendimento'>
-            <input type="checkbox" name="acesso-lento" id="acesso-lento" />
-            <label htmlFor="sem-acesso">Cliente alega acesso lento</label>
-        </div>
-        <div className='tipoatendimento'>
-            <input type="checkbox" name="cadastro" id="cadastro" />
-            <label htmlFor="sem-acesso">Possível cliente</label>
-        </div>
-        <div className='tipoatendimento'>
-            <input type="checkbox" name="fibratv" id="fibratv" />
-            <label htmlFor="sem-acesso">Cliente com dificuldade de acessar o Fibra TV</label>
-        </div>
 
-    </form>
+const PageInity = () => {
+    
+const [selecionado, setSelecionado] = useState("");
+
+const valorCapturado = (e) => {
+    setSelecionado(e.target.value);
+}
+
+const irParaTipoDeAtendimento = (e) => {
+    e.preventDefault();
+    console.log('Op:', selecionado);
+}
+
+
+  return (
+        <form onSubmit={irParaTipoDeAtendimento} className='formulario'>
+            
+            <h1>Selecione o tipo de atendimento que está se iniciando...</h1>
+            
+            <div className='tipoatendimento'>
+                <input type="radio" 
+                name="tipo-atendimento" 
+                id="sem-acesso" 
+                value='semacesso'
+                onChange={valorCapturado}
+                checked={selecionado === 'semacesso'}/>
+
+                <label htmlFor="sem-acesso">Cliente alega sem acesso</label>
+            </div>
+            
+            <div className='tipoatendimento'>
+                <input type="radio" 
+                name="tipo-atendimento" 
+                id="acesso-lento" 
+                value='acessolento'
+                onChange={valorCapturado}
+                checked={selecionado === 'acessolento'}/>
+                <label htmlFor="acesso-lento">Cliente alega acesso lento</label>
+            </div>
+            
+            <div className='tipoatendimento'>
+                <input type="radio" 
+                name="tipo-atendimento" 
+                id="cadastro" 
+                value='cadastro'
+                onChange={valorCapturado}
+                checked={selecionado === 'cadastro'}/>
+                <label htmlFor="cadastro">Possível cliente</label>
+            </div>
+            
+            <div className='tipoatendimento'>
+                <input type="radio" 
+                name="tipo-atendimento" 
+                id="fibratv" 
+                value='fibratv'
+                onChange={valorCapturado}
+                checked={selecionado === 'fibratv'}/>
+                <label htmlFor="fibratv">Cliente com dificuldade de acessar o Fibra TV</label>
+            </div>
+            
+            <div className='button'>
+                <button disabled={!selecionado} type='submit'>Confirmar</button>
+            </div>
+        </form>
+   
   )
 }
 
